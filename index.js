3c3bdfcab74f19a478f204641e2a609a22a1e50b
jahnavi yara <66204146+jahnaviyara@users.noreply.github.com>
2021-05-14 19:15:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ router.post('/getdata',function(req,res,next){
   var usertype1 = req.body.usertype;
   // console.log(usertype1);
   if(usertype1 == "Faculty"){
-    res.render('home',{});
+    res.redirect('/login')
   }
   else{
     res.redirect('/videopage');
@@ -21,7 +21,8 @@ router.post('/getdata',function(req,res,next){
 })
 
 router.get('/login',function(req, res, next){
-  res.render('home',{});
+  var status = req.query.status;
+  res.render('home',{msg:status});
 });
 
 router.get('/register',function(req, res, next){
@@ -29,54 +30,69 @@ router.get('/register',function(req, res, next){
   res.render('register',{msg:status});
 });
 
+router.get('/forgetpswd',function(req,res,next){
+  var status = req.query.status;
+  res.render('forgetpswd',{msg:status});
+})
+
 router.post('/insertion',function(req,res,next){
   var name = req.body.name;
   var id = req.body.fid;
-  // console.log(req.body.pass);
-  bcrypt.hash(req.body.pass,10,function(err,hashedPass){
-    if(err){
-      console.log(err);
-    }
-    // console.log(name,id,hashedPass);
-    db.Faculty.insert({name:name,fid:id,password:hashedPass},function(err,docs){
+  console.log("name:",name,id,req.body.pass);
+  if(name.length == 0 ||  id.length == 0 || req.body.pass.length == 0){
+    res.render('register',{msg:"Enter all credentials"})
+  }
+  else{
+    bcrypt.hash(req.body.pass,10,function(err,hashedPass){
       if(err){
         console.log(err);
       }
-      else{
-        res.redirect('/login');
-      }
-    })
-  });
+      // console.log(name,id,hashedPass);
+      db.Faculty.insert({name:name,fid:id,password:hashedPass},function(err,docs){
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.redirect('/login');
+        }
+      })
+    });
+  }
 });
 
 router.post('/validation',function(req,res,next){
   var id = req.body.fid;
   var pd = req.body.pswd;
   // console.log(id,pd);
-  db.Faculty.find({fid:id},function(err,docs){
-    if(err){
-      res.send("Please Enter Correct Credentials");
-    }
-    else{
-      // console.log(docs[0].password);
-      if(docs.length == 0){
-        res.redirect('/register?status=Not a registered user!!');
+  if(id.length == 0 || pd.length == 0){
+    res.redirect('/login?status=Enter all Credentials!!');
+  }
+  else{
+    db.Faculty.find({fid:id},function(err,docs){
+      if(err){
+        res.send("Please Enter Correct Credentials");
       }
       else{
-        bcrypt.compare(pd,docs[0].password,function(err,result){
-          if(err){
-            res.send("Enter correct Credentials"+err)
-          }
-          if(result){
-            res.render('form1',{})
-          }
-          else{
-            res.send("Enter correct Credentials")
-          }  
-        });
+        // console.log(docs[0].password);
+        if(docs.length == 0){
+          res.redirect('/register?status=Not a registered user!!');
+        }
+        else{
+          bcrypt.compare(pd,docs[0].password,function(err,result){
+            if(err){
+              res.send("Enter correct Credentials"+err)
+            }
+            if(result){
+              res.render('form1',{})
+            }
+            else{
+              res.redirect('/login?status=Enter all Credentials!!');
+            }
+          });
+        }
       }
-    }
-  })
+    });
+  }
 });
 
 router.post('/selection',function(req,res,next){
@@ -97,42 +113,26 @@ router.get('/videopage',function(req,res,next){
     }
     else{
         var links = new Array();
-        // console.log(docs);
         docs.forEach(element => {
+          // console.log(element);
           var li;
           if(element.url.includes('youtube') || element.url.includes('youtu.be')){
-            li = element.url+"/"+element.topic+"/"+element.dateofupload;
+            li = element.url+"/"+element.topic+"/"+element.dateofupload+"/"+element.subjectname;
           }
           else{
-            li = element.url+"////"+element.topic+"////"+element.dateofupload;
+            li = element.url+"////"+element.topic+"////"+element.dateofupload+"////"+element.subjectname;
           }
-          // console.log(li);
-          var scode = element.subjectcode;
-          // console.log(scode)
-          var sname;
-          db.Subject.find({subjectcode:scode},function(err,docs){
-            if(err){
-              res.send(err);
-            }
-            else{
-              // console.log(docs[0].subjectname);
-              // console.log(li);
-              sname = docs[0].subjectname;
-              // console.log(li);
-            }
-          });
-          // console.log(sname);
           links.push(li);
         });
-        // console.log(links);
-        // shuffle(links);
-        res.render('homepage',{links:links});
+        links = JSON.stringify(links);
+        res.render('videospage',{link:links});
     }
   })
 })
 
 router.get('/uploadVideo',function(req,res,next){
-  res.render('uploaddata',{});
+  var status = req.query.status;
+  res.render('uploaddata',{msg:status});
 })
 
 router.post('/datauploaded',function(req,res,next){
@@ -143,12 +143,16 @@ router.post('/datauploaded',function(req,res,next){
   var topic = req.body.topic;
   var date = req.body.date;
   var url = req.body.url;
-  db.Subject.insert({subjectname:sname,subjectcode:scode,branch:branch,semester:sem},function(err,docs){
+  if(sname.length == 0 || scode.length == 0 || branch.length == 0 || sem.length == 0 || topic.length == 0 || date.length == 0 || url.length == 0){
+    res.redirect('/uploadVideo?status=All the data is needed!!');
+  }
+  else{
+    db.Subject.insert({subjectname:sname,subjectcode:scode,branch:branch,semester:sem},function(err,docs){
       if(err){
         res.send(err)
       }
       else{
-        db.Videos.insert({subjectcode:scode,topic:topic,dateofupload:date,url:url},function(err,docs){
+        db.Videos.insert({subjectcode:scode,topic:topic,dateofupload:date,subjectname:sname,url:url},function(err,docs){
           if(err){
             res.send(err);
           }
@@ -158,6 +162,77 @@ router.post('/datauploaded',function(req,res,next){
         })
       }
   });
+  }
 });
 
+router.post('/SearchBar',function(req,res,next){
+  var ttbs = req.body.ttbs;
+  if(ttbs.length == 0){
+    res.redirect('/videopage');
+  }
+  else{
+    db.Videos.createIndex( { subjectname: "text", topic: "text",dateofupload: "text" });
+    db.Videos.find({$text: {$search : ttbs}},function(err,docs){
+      if(err){
+        res.send(err);
+      }
+      else{
+        var links = new Array();
+        if(docs.length == 0){
+          res.redirect('/videopage');
+        }
+        else{
+          docs.forEach(element => {
+            // console.log(element);
+            var li;
+            if(element.url.includes('youtube') || element.url.includes('youtu.be')){
+              li = element.url+"/"+element.topic+"/"+element.dateofupload+"/"+element.subjectname;
+            }
+            else{
+              li = element.url+"////"+element.topic+"////"+element.dateofupload+"////"+element.subjectname;
+            }
+            links.push(li);
+          });
+          links = JSON.stringify(links);
+          res.render('videospage',{link:links});
+        }
+      }
+    })
+  }
+});
+
+router.post('/forgetpassword',function(req,res,next){
+  var fid = req.body.fid;
+  var pass1 = req.body.pass1;
+  var pass = req.body.pass;
+  // console.log(pass,pass1);
+  if(pass1.length == 0 || pass.length == 0){
+    res.render('forgetpswd',{msg:"Please Enter Password"}) 
+  }
+  else{
+    if(pass1 != pass){
+      res.render('forgetpswd',{msg:"Enter same password in confirm password and new password."})
+    }
+    else{
+      bcrypt.hash(req.body.pass1,10,function(err,hashedPass){
+        if(err){
+          console.log(err);
+        }
+        db.Faculty.findAndModify({query:{fid:fid},update:{$set:{password:hashedPass}},new:false},function(err,docs){
+          if(err){
+            res.send(err);
+          }
+          else{
+            if(docs == null){
+              res.render('register',{msg: "You are not a registered user"});
+            }
+            else{
+              res.redirect('/login');
+            }
+          }
+        })
+      });
+    }
+  }
+});
 module.exports = router;   
